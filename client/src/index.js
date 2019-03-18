1d8de27674bcd294091ba5066e0c95791cc7d868
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-18 17:34:40 -0400

    updated index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import store from './store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import createBrowserHistory from 'history/createBrowserHistory';
+
+export const history = createBrowserHistory();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Routes history={history} />
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
