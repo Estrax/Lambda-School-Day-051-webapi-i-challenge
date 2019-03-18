8619e9f5a0a4411bbaa69b1a5796c1f697dc1fd9
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-18 15:35:21 -0400

    updated bodyparser and routes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = require('./router');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 const PORT = process.env.PORT || 5000;
 
 app.use('/api', router);
--- router.js
+++ router.js
@@ -4,12 +4,23 @@ const db = require('./data/db');
 
 router.route('/users')
     .get((req, res) => {
-        
+        db
+            .find()
+            .then(response => res.status(200).json(response))
+            .catch(err => res.status(500).json({ error: "The users information could not be retrieved." }));
     })
     .post((req, res) => {
         const {name, bio, created_at, updated_at} = req.body;
         if(!name || !bio) res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
-        
+        db
+            .insert({
+                name,
+                bio,
+                created_at,
+                updated_at
+            })
+            .then(response => res.status(201).json(response))
+            .catch(err => res.status(500).json({ error: "There was an error while saving the user to the database" }));
     });
 
 router.route('/users/:id')
