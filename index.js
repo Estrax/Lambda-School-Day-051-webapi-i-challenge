879f56045cb8dbb51270543373130518c6849da9
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-18 15:09:53 -0400

    added basic webserver

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // implement your API here
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, (err) => {
+    if(err) throw err;
+    console.log(`App running on port ${PORT}`);
+})
