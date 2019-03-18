91efeb480c30eb019304b9b80702c14b683ad8bc
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-18 15:23:42 -0400

    added db connector

--- router.js
+++ router.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const db = require('./data/db');
 
 router.route('/users')
     .get((req, res) => {
         
     })
     .post((req, res) => {
+        const {name, bio, created_at, updated_at} = req.body;
+        if(!name || !bio) res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
         
     });
 
