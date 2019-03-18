664345d4226a593aedefc7ac47ff7b66b9649b5b
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-18 15:14:08 -0400

    added basic routes

--- router.js
+++ router.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+router.route('/users')
+    .get((req, res) => {
+        
+    })
+    .post((req, res) => {
+        
+    });
+
+router.route('/users/:id')
+    .get((req, res) => {
+
+    })
+    .put((req, res) => {
+
+    })
+    .delete((req, res) => {
+
+    });
 
 module.exports = router;
