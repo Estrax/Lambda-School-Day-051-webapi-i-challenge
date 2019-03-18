b06c4ed8efb0ce77c73a22c19a423153d97f8352
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-18 15:11:31 -0400

    added router bindings

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // implement your API here
 const express = require('express');
+const router = require('./router');
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
+
+app.use('/api', router);
 
 app.listen(PORT, (err) => {
     if(err) throw err;
